16dc0ef3c0e0c68ae6cfd6639199947bed9d6f66
Francilino-Netoo <francilino12345@gmail.com>
2024-06-20 15:28:25 -0300

    ajustado

--- android/app/src/main/java/com/barbeiro/MainActivity.kt
+++ android/app/src/main/java/com/barbeiro/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.barbeiro
+package com.barbeiroapp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "barbeiro"
+  override fun getMainComponentName(): String = "barbeiroapp"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/barbeiro/MainApplication.kt
+++ android/app/src/main/java/com/barbeiro/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.barbeiro
+package com.barbeiroapp
 
 import android.app.Application
 import com.facebook.react.PackageList
